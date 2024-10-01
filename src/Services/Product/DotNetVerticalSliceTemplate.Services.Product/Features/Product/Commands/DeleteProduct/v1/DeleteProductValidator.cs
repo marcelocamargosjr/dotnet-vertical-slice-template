1a236c35cbf0acccf4fff7f223315ab09d0134ff
Marcelo Camargos <marcelo.camargos@abundancebrasil.com>
2024-10-01 12:43:49 -0300

    3

---
+++ src/Services/Product/DotNetVerticalSliceTemplate.Services.Product/Features/Product/Commands/DeleteProduct/v1/DeleteProductValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetVerticalSliceTemplate.Services.Product.Features.Product.Commands.DeleteProduct.v1
+{
+    internal class DeleteProductValidator
+    {
+    }
+}
