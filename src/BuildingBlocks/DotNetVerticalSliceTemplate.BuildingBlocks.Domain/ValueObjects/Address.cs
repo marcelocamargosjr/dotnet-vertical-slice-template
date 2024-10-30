bf083aa4d0a0979dc7f26b53b07171c278305947
Marcelo Camargos <marcelo.camargos@abundancebrasil.com>
2024-10-30 17:31:10 -0300

    8

---
+++ src/BuildingBlocks/DotNetVerticalSliceTemplate.BuildingBlocks.Domain/ValueObjects/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetVerticalSliceTemplate.BuildingBlocks.Domain.ValueObjects;
+
+public record Address
+{
+    public string Street { get; private set; }
+
+    public string City { get; private set; }
+
+    public string ZipCode { get; private set; }
+
+    public Address(string street, string city, string zipCode)
+    {
+        Street = street;
+        City = city;
+        ZipCode = zipCode;
+    }
+}
