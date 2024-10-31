0cfdeabaad81f4d1e01358a5be7e748814c32219
Marcelo Camargos <marcelo.camargos@abundancebrasil.com>
2024-10-30 22:51:28 -0300

    9

---
+++ src/BuildingBlocks/DotNetVerticalSliceTemplate.BuildingBlocks.Domain/Repositories/IConcurrencyHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetVerticalSliceTemplate.BuildingBlocks.Domain.Repositories;
+
+public interface IConcurrencyHandler<in TEntity>
+{
+    void SetRowVersion(TEntity entity, byte[] version);
+
+    bool IsDbUpdateConcurrencyException(Exception ex);
+}
